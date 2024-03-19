12b37ae525a90e6d36d0fed0c1fe0fad59d0d891
matheus rodrigues monte <matheusr.monte2004@gmail.com>
2024-03-19 14:12:38 -0300

    lista 2 completa

--- routes/lista2.js
+++ routes/lista2.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+router.post('/ex1',function(req,res){
+    const minimo = Number(req.body.minimo);
+    const maximo = Number(req.body.maximo);
+    
+    const estoqueMedio = (minimo + maximo) / 2;
+    
+   res.json({estoqueMedio})
+    
+      
 
-router.post('/ex1', function(req,res){  
-    
-    
-    res.json({})
 })
+
 
 
 router.post('/ex2', function (req, res) {
@@ -19,4 +25,92 @@ router.post('/ex2', function (req, res) {
     res.json({ nome,salarioBruto, bonificacaoFilhos, salarioFinal })
 })
 
+router.post('/ex3',function(req,res){
+    const {idadeAnos, idadeMes, idadeDia} = req.body
+
+    const totalDias = (idadeAnos * 365) + (idadeMes * 30) + idadeDia
+
+    res.json({totalDias})
+
+})
+
+router.post('/ex4', function (req, res) {
+
+
+    let idade = req.body.idade
+
+    let idadeAnos = idade / 365 | 0;
+    idade -= idadeAnos * 365;
+    let idadeMeses = idade / 30 | 0;
+    idade -= idadeMeses * 30;
+
+    const idadeDias = idade
+
+    res.json({ idadeAnos, idadeMeses, idadeDias })
+})
+
+
+router.post('/ex5', function (req, res) {
+    const { n1, n2, n3 } = req.body
+
+    const media = Number(n1 * 2) + Number(n2 * 3) + Number(n3 * 5) / 10
+
+    
+    res.json({ media,})
+
+
+})
+
+router.post('/ex6', function (req, res) {
+
+    let tempoSegundos, tempominutos, tempohoras;
+    tempoSegundos = req.body.tempoSegundos
+
+    tempominutos = tempoSegundos / 60
+    tempohoras = tempominutos / 60
+
+    res.json({ tempoSegundos, tempohoras, tempominutos })
+
+
+})
+
+router.post('/ex7', function (req, res) {
+    const { nome, salariofixo, totalVendas, percentual } = req.body
+
+    salario = salariofixo + (totalVendas * percentual)
+
+    res.json({ nome, salario })
+
+})
+
+
+router.post('/ex8', function (req, res) {
+    const { nomePiloto, distacia, tempoHora, } = req.body
+
+    VM = distacia / tempoHora
+
+    res.json(`A velocidade alcançada pelo ${nomePiloto} foi de ${VM}km/hs`)
+
+})
+
+
+router.post('/ex9', function (req, res) {
+    salarioFuncionario = req.body.salarioFuncionario
+    novosalario = 0
+
+    if(salarioFuncionario < 1000)
+    {
+        novosalario =  salarioFuncionario * 1.3
+    }
+
+    if(novosalario > 0){
+        resultado = `seu novo salário é ${novosalario}`
+    }
+
+    else {
+        resultado = `você não está apto a receber seu novo salário`
+    }
+
+    res.json(`mensagem : ${resultado}`)
+})
 module.exports = router
